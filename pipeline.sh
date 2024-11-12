2253d1d66b2aca54066463d09683cff2ff903d12
Kyle Kurkela <kkurkela@bu.edu>
2024-11-12 15:43:52 -0500

    added Brian's cleanup code. Removed unnecessary move files step

--- pipeline.sh
+++ pipeline.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
+function cleanup {
+  # cleanup the temp directory
+  rm -Rf /tmp/bids
+  rm -Rf /tmp/work
+  rm -Rf /tmp/derivatives
+}
+
+# trap <function-to-run> <signal>
+# 0 means 'all signals' like the one that is recieved when the script reaches its end.
+# this should catch successful run throughs and crashes
+trap cleanup 0
+
 # step 1 -- pull down the config file
 python download_bidsconfig.py --host $XNAT_HOST --user $XNAT_USER --password $XNAT_PASS
 
 # step 2 -- BIDSIFY
-dcm2bids -d /input/DICOM -p 01 -c bids_config.json -o /tmp/bids
+dcm2bids -d /input/DICOM -p subject -c bids_config.json -o /tmp/bids
 
 # step 3 -- run mriqc
-NCORES=`nproc --all`
-mriqc /tmp/bids/ /tmp/derivatives/ participant -w /tmp/work/ --no-sub --notrack --nprocs $NCORES --omp-nthreads $NCORES $1
 
-# step 6 -- place mriqc output to XNAT
+# number of available cores
+NCORES=`nproc --all
+
+# make a output subdirectory for one click downloading
 mkdir /output/report
-mv /tmp/derivatives/sub-01*.html /output/report/mriqc_report.html
-mv /tmp/derivatives/sub-01 /output/report
 
-# step 7 -- cleanup
-rm -Rf /tmp/bids
-rm -Rf /tmp/derivatives
-rm -Rf /tmp/work
+# mriqc call
+mriqc /tmp/bids/ /output/report/ participant -w /tmp/work/ --no-sub --notrack --nprocs $NCORES --omp-nthreads $NCORES $1
+
+# rename the report to make it clear what it is
+mv /output/report/sub-subject.html /output/report/mriqc_report.html
